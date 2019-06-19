36795796b27d946338351c8616cd268d324ac929
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-06-19 14:18:41 +0200

    add notifsActions file

---
+++ app/actions/notificationsActions.js
@@ -0,0 +1,42 @@
+import firebase from "../firebase/firebase";
+
+class NotifsActions {
+  async DELETE_NOTIFICATION(user_uid, notif_uid) {
+    firebase.db
+      .collection("users")
+      .doc(user_uid)
+      .collection("notifications")
+      .doc(notif_uid)
+      .delete();
+  }
+  async FOLLOW_REQUEST_ACCEPTED(newFriend_uid, newFriend_avatar) {
+    firebase.db
+      .collection("users")
+      .doc(newFriend_uid)
+      .collection("notifications")
+      .add({
+        type: "follow_request_accepted",
+        user: {
+          uid: firebase.auth.currentUser.uid,
+          username: firebase.auth.currentUser.displayName,
+          avatar: newFriend_avatar
+        }
+      });
+  }
+  async NEW_FRIEND(newFriend) {
+    firebase.db
+      .collection("users")
+      .doc(firebase.auth.currentUser.uid)
+      .collection("notifications")
+      .add({
+        type: "new_friend",
+        user: {
+          username: newFriend.user.username,
+          avatar: newFriend.user.avatar,
+          uid: newFriend.user.uid
+        }
+      });
+  }
+}
+
+export default new NotifsActions();
