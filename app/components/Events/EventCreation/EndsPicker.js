d52b2a16994e436f640f6c908ffad0dbabb9f94c
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-06-28 15:40:56 +0200

    create news starts / ends time picker

---
+++ app/components/Events/EventCreation/EndsPicker.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import DateTimePicker from "react-native-modal-datetime-picker";
+
+export default class Event_EndsPicker extends Component {
+  render() {
+    return (
+      <DateTimePicker
+        mode={!this.props.isFullDay ? "datetime" : "date"}
+        date={this.props.ends.date}
+        isVisible={this.props.isVisibleEnds}
+        // isVisible={this.props.isVisibleDate}
+        onConfirm={ends => this.props.handleEndsPicked(ends)}
+        onCancel={this.props.hideEndsPicker}
+      />
+    );
+  }
+}
