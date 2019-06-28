ce7ca85944eeed9f6697c142d6501c140402410b
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-06-28 15:41:55 +0200

    improve UI

--- app/components/Events/EventCreation/Middle.js
+++ app/components/Events/EventCreation/Middle.js
@@ -9,6 +9,10 @@ import {
   TouchableWithoutFeedback,
   TouchableOpacity
 } from "react-native";
+import { Hr } from "../../Hr/styles";
+const moment = require("moment");
+import Event_StartsPicker from "./StartsPicker";
+import Event_EndsPicker from "./EndsPicker";
 import { Input } from "native-base";
 import { Icon } from "expo";
 
@@ -22,16 +26,181 @@ export default class MiddleCreation extends React.Component {
   }
   render() {
     return (
-      <TouchableWithoutFeedback
-        onPress={() => {
-          Keyboard.dismiss();
-        }}
-      >
-        <KeyboardAvoidingView behavior="position" enabled>
-          <View style={{ paddingHorizontal: 25, marginBottom: 20 }}>
-            <TextInput
+      <View>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            Keyboard.dismiss();
+          }}
+        >
+          <KeyboardAvoidingView behavior="position" enabled>
+            <View
+              style={{ marginTop: 20, paddingHorizontal: 25, marginBottom: 40 }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  marginTop: 25,
+                  // marginLeft: 5,
+                  alignItems: "center"
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 18,
+                    fontWeight: "600",
+                    color: "black"
+                  }}
+                >
+                  Title
+                </Text>
+                <View
+                  style={{
+                    flexDirection: "collumn",
+                    left: 145,
+                    position: "absolute"
+                  }}
+                >
+                  {/* <TextInput
+                  multiline={false}
+                  style={styles.subTitle}
+                  placeholder={this.props.name}
+                  autoFocus={false}
+                  returnKeyType="go"
+                  selectionColor={"#1DC161"}
+                  onChangeText={(e, stateName) =>
+                    this.props.setInputsStates(e, "name")
+                  }
+                /> */}
+                  <TextInput
+                    multiline={false}
+                    style={styles.subTitle}
+                    placeholder={`${this.props.username}'s event`}
+                    autoFocus={true}
+                    returnKeyType="next"
+                    selectionColor={"#1DC161"}
+                    onChangeText={(e, stateName) =>
+                      this.props.setInputsStates(e, "title")
+                    }
+                  />
+                  <View
+                    style={{
+                      top: 11,
+                      height: 0.5,
+                      width: 215,
+                      backgroundColor: "rgba(0, 0, 0, 0.2)"
+                      // "black"
+                      // borderColor: "rgba(0, 0, 0, 0.1)"
+                      // borderWidth: 0.6
+                    }}
+                  />
+                </View>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  marginTop: 25,
+                  // marginLeft: 5,
+                  alignItems: "center"
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 18,
+                    fontWeight: "600",
+                    color: "black"
+                  }}
+                >
+                  Description
+                </Text>
+                <View
+                  style={{
+                    flexDirection: "collumn",
+                    left: 145,
+                    position: "absolute"
+                  }}
+                >
+                  {/* <TextInput
+                  multiline={false}
+                  style={styles.subTitle}
+                  placeholder={this.props.name}
+                  autoFocus={false}
+                  returnKeyType="go"
+                  selectionColor={"#1DC161"}
+                  onChangeText={(e, stateName) =>
+                    this.props.setInputsStates(e, "name")
+                  }
+                /> */}
+                  <TextInput
+                    multiline={false}
+                    style={styles.subTitle}
+                    placeholder={"Description of the event"}
+                    autoFocus={false}
+                    returnKeyType="next"
+                    selectionColor={"#1DC161"}
+                    onChangeText={(e, stateName) =>
+                      this.props.setInputsStates(e, "text")
+                    }
+                  />
+                  <View
+                    style={{
+                      top: 11,
+                      height: 0.5,
+                      width: 215,
+                      backgroundColor: "rgba(0, 0, 0, 0.2)"
+                      // "black"
+                      // borderColor: "rgba(0, 0, 0, 0.1)"
+                      // borderWidth: 0.6
+                    }}
+                  />
+                </View>
+              </View>
+              <TouchableOpacity
+                style={{
+                  flexDirection: "row",
+                  marginTop: 25,
+                  // marginLeft: 5,
+                  alignItems: "center"
+                }}
+                onPress={() =>
+                  this.props.navigation.navigate("LocationPicker", {
+                    setUpLocation: data => this.props.setUpLocation(data)
+                  })
+                }
+              >
+                <Text
+                  style={{
+                    fontSize: 18,
+                    fontWeight: "600",
+                    color: "black"
+                  }}
+                >
+                  Location
+                </Text>
+                <Text
+                  style={[
+                    styles.subTitle,
+                    {
+                      left: 145,
+                      position: "absolute",
+                      color:
+                        this.props.location.description !== ""
+                          ? "black"
+                          : "rgba(0,0,0,0.23)"
+                    }
+                  ]}
+                >
+                  {this.props.location.description !== ""
+                    ? this.props.location.description.length > 25
+                      ? this.props.location.description.slice(0, 25) + "..."
+                      : this.props.location.description
+                    : // ? this.props.location.description.split(",")[0]
+                      // : this.props.location.description.split(",")[0]
+                      "Current location"}
+                </Text>
+              </TouchableOpacity>
+              {/* <TextInput
               multiline={true}
-              style={styles.mainTitle}
+              style={styles.subTitle}
               placeholder={"Title"}
               autoFocus={false}
               returnKeyType="next"
@@ -50,14 +219,14 @@ export default class MiddleCreation extends React.Component {
               onChangeText={(e, stateName) =>
                 this.props.setInputsStates(e, "text")
               }
-            />
-            {/* <Text style={styles.subTitle}>
+            /> */}
+              {/* <Text style={styles.subTitle}>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
               ipsum repellendus esse doloremque natus corporis culpa quibusdam
               hic. Ad obcaecati deserunt exercitationem quasi et sed tempore
               rerum ducimus reiciendis maxime.
             </Text> */}
-            <View
+              {/* <View
               style={{
                 marginTop: 15,
                 flexDirection: "row",
@@ -112,10 +281,231 @@ export default class MiddleCreation extends React.Component {
                   // </Button>
                 ))}
               </View>
+            </View> */}
+            </View>
+            <Hr style={{ marginLeft: 0, marginRight: 0 }} />
+
+            {/* <Hr style={{ marginLeft: 0, marginRight: 0 }} /> */}
+            <Event_StartsPicker {...this.props} />
+            <Event_EndsPicker {...this.props} />
+          </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+        <View
+          style={{ marginTop: 20, paddingHorizontal: 25, marginBottom: 20 }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 25,
+              // marginLeft: 5,
+              alignItems: "center"
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: "600",
+                color: "black"
+              }}
+            >
+              Full day
+            </Text>
+            <View
+              style={{
+                flexDirection: "collumn",
+                left: 145,
+                position: "absolute"
+              }}
+            >
+              <Icon.Feather
+                style={{ left: 175 }}
+                name={!this.props.isFullDay ? "toggle-left" : "toggle-right"}
+                size={40}
+                color={!this.props.isFullDay ? "rgba(0, 0, 0, 0.2)" : "#1DC161"}
+                onPress={this.props.handleFullDay}
+              />
+              <View
+                style={{
+                  top: 4.5,
+                  height: 0.5,
+                  width: 215,
+                  backgroundColor: "rgba(0, 0, 0, 0.2)"
+                  // "black"
+                  // borderColor: "rgba(0, 0, 0, 0.1)"
+                  // borderWidth: 0.6
+                }}
+              />
             </View>
           </View>
-        </KeyboardAvoidingView>
-      </TouchableWithoutFeedback>
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              marginTop: 25,
+              // marginLeft: 5,
+              alignItems: "center"
+            }}
+            onPress={this.props.showStartsPicker}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: "600",
+                color: "black"
+              }}
+            >
+              Starts
+            </Text>
+            <View
+              style={{
+                flexDirection: "collumn",
+                left: 145,
+                position: "absolute"
+              }}
+            >
+              {this.props.isFullDay ? (
+                <Text
+                  style={[
+                    styles.subTitle,
+                    {
+                      fontSize: 18,
+                      color: "#1DC161",
+                      fontWeight: "600"
+                    }
+                  ]}
+                >
+                  {moment(this.props.starts.date).format("dddd ll")}
+                </Text>
+              ) : (
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    width: 215
+                  }}
+                >
+                  <Text
+                    style={[
+                      styles.subTitle,
+                      {
+                        // left: 145,
+                        fontSize: 18,
+                        // position: "absolute",
+                        color: "#1DC161",
+                        fontWeight: "600"
+                      }
+                    ]}
+                  >
+                    {moment(this.props.starts.date).format("ll")}
+                  </Text>
+                  {!this.props.isFullDay ? (
+                    <Text
+                      style={[
+                        styles.subTitle,
+                        {
+                          // position: "absolute",
+                          color: "#1DC161",
+                          fontWeight: "600"
+                        }
+                      ]}
+                    >
+                      {moment(this.props.starts.time).format("LT")}
+                    </Text>
+                  ) : null}
+                </View>
+              )}
+              <View
+                style={{
+                  top: 11,
+                  height: 0.5,
+                  width: 215,
+                  backgroundColor: "rgba(0, 0, 0, 0.2)"
+                }}
+              />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              marginTop: 25,
+              alignItems: "center"
+            }}
+            onPress={this.props.showEndsPicker}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: "600",
+                color: "black"
+              }}
+            >
+              Ends
+            </Text>
+            {this.props.isFullDay ? (
+              <Text
+                style={[
+                  styles.subTitle,
+                  {
+                    left: 145,
+                    fontSize: 18,
+                    position: "absolute",
+                    color: "#1DC161",
+                    fontWeight: "600"
+                  }
+                ]}
+              >
+                {moment(this.props.ends.date).isBefore(
+                  moment(this.props.starts.date)
+                )
+                  ? moment(this.props.starts.date).format("dddd ll")
+                  : moment(this.props.ends.date).format("dddd ll")}
+              </Text>
+            ) : (
+              <View
+                style={{
+                  left: 145,
+                  position: "absolute",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  width: 215
+                }}
+              >
+                <Text
+                  style={[
+                    styles.subTitle,
+                    {
+                      fontSize: 18,
+                      color: "#1DC161",
+                      fontWeight: "600"
+                    }
+                  ]}
+                >
+                  {moment(this.props.ends.date).isBefore(
+                    moment(this.props.starts.date)
+                  )
+                    ? moment(this.props.starts.date).format("ll")
+                    : moment(this.props.ends.date).format("ll")}
+                </Text>
+                <Text
+                  style={[
+                    styles.subTitle,
+                    {
+                      fontSize: 18,
+                      color: "#1DC161",
+                      fontWeight: "600"
+                    }
+                  ]}
+                >
+                  {moment(this.props.ends.time).isBefore(
+                    moment(this.props.starts.time)
+                  )
+                    ? moment(this.props.starts.time).format("LT")
+                    : moment(this.props.ends.time).format("LT")}
+                </Text>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
@@ -128,7 +518,6 @@ const styles = StyleSheet.create({
     color: "black"
   },
   subTitle: {
-    marginTop: 30,
     fontSize: 18,
     lineHeight: 27,
     color: "black",
