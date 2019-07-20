8f4db209503fcee9410832317e2030a266cfd814
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-07-20 23:33:50 +0200

    improve location picker + starting add listed locations system

--- app/components/Events/EventCreation/LocationPicker.js
+++ app/components/Events/EventCreation/LocationPicker.js
@@ -12,6 +12,7 @@ import { Icon } from "expo";
 import { screenWidth } from "../../../utils/dimensions";
 import firebase from "../../../firebase/firebase";
 import UsersActions from "../../../actions/usersActions";
+const moment = require("moment");
 
 // const homePlace = {
 //   description: "Home",
@@ -28,8 +29,9 @@ export default class LocationPicker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      favorite_locations: [],
+      listed_locations: [],
       isFavoriteLocationsLoaded: false,
+      isListedLocationsLoaded: true,
       isViewList: false
     };
   }
@@ -40,13 +42,15 @@ export default class LocationPicker extends React.Component {
     firebase.db
       .collection("users")
       .doc(firebase.auth.currentUser.uid)
-      .collection("favorite_locations")
+      .collection("listed_locations")
       .onSnapshot(() =>
-        UsersActions.GET_USER_FAVORITE_LOCATIONS(
+        UsersActions.GET_USER_LISTED_LOCATIONS(
           firebase.auth.currentUser.uid
         ).then(locations =>
-          this.setState({ favorite_locations: locations }, () =>
-            console.log(this.state.favorite_locations)
+          this.setState(
+            { listed_locations: locations }
+            // , () =>
+            // console.log(this.state.listed_locations)
           )
         )
       );
@@ -56,36 +60,6 @@ export default class LocationPicker extends React.Component {
   }
   render() {
     return (
-      // <View
-      //   style={{
-      //     backgroundColor: "#F7F7F7"
-      //   }}
-      // >
-      //   <View
-      //     style={{
-      //       marginTop: 55,
-      //       marginBottom: 15,
-      //       flexDirection: "row",
-      //       justifyContent: "center"
-      //     }}
-      //   >
-      //     <Text style={{ fontWeight: "500", fontSize: 16 }}>Location</Text>
-      //     <Button
-      //       style={{
-      //         position: "absolute",
-      //         left: screenWidth - 50,
-      //         top: -7,
-      //         height: 35,
-      //         width: 35,
-      //         borderRadius: 10,
-      //         backgroundColor: "rgba(0, 0, 0, 0.04)",
-      //         justifyContent: "center"
-      //       }}
-      //       onPress={() => this.props.navigation.navigate("EventCreationView")}
-      //     >
-      //       <Icon.Ionicons name="ios-arrow-round-up" size={25} color="black" />
-      //     </Button>
-      //   </View>
       <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
         <View>
           <View
@@ -105,33 +79,13 @@ export default class LocationPicker extends React.Component {
               <Text style={{ fontWeight: "500", fontSize: 16 }}>Location</Text>
             </View>
             <View style={{ flexDirection: "row" }}>
-              {/* <TextInput
-              style={{
-                width: screenWidth - 75,
-                height: 36,
-                marginRight: 15,
-                marginLeft: 60,
-                backgroundColor: "rgba(0, 0, 0, 0.04)",
-                borderRadius: 10,
-                paddingHorizontal: 10,
-                fontSize: 18
-              }}
-              selectionColor={"#1DC161"}
-              autoCapitalize="none"
-              placeholder={"search"}
-              autoFocus={false}
-              returnKeyType="next"
-              value={this.state.search}
-              onChangeText={e => this.setState({ search: e })}
-            /> */}
-
               <GooglePlacesAutocomplete
                 isViewListFunction={text =>
                   text.length >= 2
                     ? this.setState({ isViewList: true })
                     : this.setState({ isViewList: false })
                 }
-                favorite_locations={this.state.favorite_locations}
+                listed_locations={this.state.listed_locations}
                 placeholder={
                   this.props.navigation.getParam("location").description
                 }
@@ -157,11 +111,18 @@ export default class LocationPicker extends React.Component {
                     data,
                     add_info
                   );
+                  UsersActions.ADD_TO_LISTED_LOCATIONS({
+                    // isFavorite: true,
+                    ...data,
+                    ...add_info,
+                    saved_date: moment().format()
+                  });
                   this.props.navigation.navigate("EventCreationView");
                   // 'details' is provided when fetchDetails = true
                   // console.log(data);
                 }}
                 isFavoriteLocationsLoaded={this.state.isFavoriteLocationsLoaded}
+                isListedLocationsLoaded={this.state.isListedLocationsLoaded}
                 getDefaultValue={() =>
                   this.props.navigation.getParam("location").description !==
                   "search location"
@@ -173,40 +134,25 @@ export default class LocationPicker extends React.Component {
                   key: "AIzaSyCjg_ds0yIsaxR4C2bvS0PksRey8QqqIoY",
                   language: "en", // languagea of the results
                   type: "geocode"
-                  // region: "FR"
-                  // type: "(cities)"
                 }}
-                // selectionColor={"#1DC161"}
                 styles={{
                   textInputContainer: {
                     borderTopWidth: 0,
                     borderBottomWidth: 0,
-                    // backgroundColor: "#F7F7F7",
                     paddingHorizontal: 10
                   },
                   textInput: {
                     bottom: 8,
                     width: screenWidth - 75,
                     height: 36,
-                    // marginRight: 15,
                     marginLeft: 50,
-                    // backgroundColor: "#F7F7F7",
-
                     backgroundColor: "rgba(232, 233, 232, 1)",
                     borderRadius: 10,
                     paddingHorizontal: 10,
                     fontSize: 18
-                    // height: 36,
-                    // marginRight: 5,
-                    // marginLeft: 5,
-                    // backgroundColor: "rgba(0, 0, 0, 0.04)",
-                    // borderRadius: 10,
-                    // paddingHorizontal: 10,
-                    // fontSize: 18
                   },
                   description: {
                     fontWeight: "bold",
-                    // marginLeft: 15,
                     fontSize: 16
                   },
                   predefinedPlacesDescription: {
@@ -215,7 +161,11 @@ export default class LocationPicker extends React.Component {
                   listView: {
                     position: "absolute",
                     zIndex: 9999,
-                    top: 132,
+                    top:
+                      this.state.listed_locations.filter(loc => loc.isFavorite)
+                        .length !== 0
+                        ? 132
+                        : 60,
                     height: 800
                   },
                   row: {
@@ -252,7 +202,9 @@ export default class LocationPicker extends React.Component {
                 //     // "administrative_area_level_3"
                 //   ]
                 // } // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-                // predefinedPlaces={[homePlace, workPlace]}
+                // predefinedPlaces={this.state.listed_locations.filter(
+                //   loc => !loc.isFavorite
+                // )}
                 debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
                 // renderLeftButton={() => (
                 //   <Image source={require("path/custom/left-icon")} />
@@ -279,7 +231,8 @@ export default class LocationPicker extends React.Component {
               </Button>
             </View>
           </View>
-          {this.state.favorite_locations.length !== 0 ? (
+          {this.state.listed_locations.filter(loc => loc.isFavorite).length !==
+          0 ? (
             <TouchableOpacity
               onPress={() =>
                 this.setState({
@@ -314,8 +267,12 @@ export default class LocationPicker extends React.Component {
                       color: "rgba(0, 0, 0, 0.2)"
                     }}
                   >
-                    {this.state.favorite_locations.length}
-                    {this.state.favorite_locations.length > 1
+                    {
+                      this.state.listed_locations.filter(loc => loc.isFavorite)
+                        .length
+                    }
+                    {this.state.listed_locations.filter(loc => loc.isFavorite)
+                      .length > 1
                       ? " places"
                       : " place"}
                   </Text>
@@ -326,9 +283,6 @@ export default class LocationPicker extends React.Component {
                   top: 14,
                   height: 0.5,
                   backgroundColor: "rgba(0, 0, 0, 0.2)"
-                  // "black"
-                  // borderColor: "rgba(0, 0, 0, 0.1)"
-                  // borderWidth: 0.6
                 }}
               />
             </TouchableOpacity>
@@ -336,90 +290,6 @@ export default class LocationPicker extends React.Component {
           {!this.state.isViewList ? (
             <View style={{ height: 1000, width: screenWidth }} />
           ) : null}
-          {/* <GooglePlacesAutocomplete
-          placeholder="Search location"
-          minLength={2} // minimum length of text to search
-          autoFocus={true}
-          returnKeyType={"search"} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
-          // keyboardAppearance={"light"} // Can be left out for default keyboardAppearance https://facebook.github.io/react-native/docs/textinput.html#keyboardappearance
-          listViewDisplayed="auto" // true/false/undefined
-          fetchDetails={true}
-          renderDescription={row =>
-            row.description || row.formatted_address || row.name
-          }
-          // renderDescription={row => row.description} // custom description render
-          onPress={(data, details = null) => {
-            this.props.navigation.getParam("setUpLocation")(data);
-            this.props.navigation.navigate("EventCreationView");
-            // 'details' is provided when fetchDetails = true
-            // console.log(data);
-          }}
-          getDefaultValue={() => ""}
-          query={{
-            // available options: https://developers.google.com/places/web-service/autocomplete
-            key: "AIzaSyCjg_ds0yIsaxR4C2bvS0PksRey8QqqIoY",
-            language: "en", // languagea of the results
-            types: "geocode" // default: 'geocode'
-          }}
-          // selectionColor={"#1DC161"}
-          styles={{
-            textInputContainer: {
-              borderTopWidth: 0,
-              backgroundColor: "#F7F7F7",
-              paddingHorizontal: 10
-            },
-            textInput: {
-              height: 36,
-              marginRight: 5,
-              marginLeft: 5,
-              backgroundColor: "rgba(0, 0, 0, 0.04)",
-              borderRadius: 10,
-              paddingHorizontal: 10,
-              fontSize: 18
-            },
-            description: {
-              fontWeight: "bold"
-            },
-            predefinedPlacesDescription: {
-              color: "#1DC161"
-            },
-            listView: {
-              position: "absolute",
-              zIndex: 9999,
-              top: 40
-            },
-            row: {
-              backgroundColor: "white"
-            },
-            separator: { marginLeft: 20, marginRight: 20 }
-          }}
-          currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
-          currentLocationLabel="Current location"
-          nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-          GoogleReverseGeocodingQuery={
-            {
-              // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-            }
-          }
-          GooglePlacesSearchQuery={{
-            // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-            rankby: "distance",
-            type: "cafe"
-          }}
-          GooglePlacesDetailsQuery={{
-            // available options for GooglePlacesDetails API : https://developers.google.com/places/web-service/details
-            fields: "formatted_address"
-          }}
-          filterReverseGeocodingByTypes={[
-            "locality",
-            "administrative_area_level_3"
-          ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-          // predefinedPlaces={[homePlace, workPlace]}
-          debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
-          // renderLeftButton={() => (
-          //   <Image source={require("path/custom/left-icon")} />
-          // )}
-        />*/}
         </View>
       </TouchableWithoutFeedback>
     );
