07762d0d6cfd4ba264cb17101a2e6f2a20b6a263
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-11-27 19:01:33 +0100

    install expo SDK35

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
-import React from "react";
-import FirebaseInitialization from "./app/firebase/firebaseInitialization";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return <FirebaseInitialization />;
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
